6ea365aadabb818331868aaebfca355da9f6fdad
mariquegonn <ocatodev@gmail.com>
2023-11-08 18:48:45 -0300

    feat: Add new icons: User, Bookmarks, Gear, and Sign Out.

--- src/components/icons/icons.tsx
+++ src/components/icons/icons.tsx
@@ -5,4 +5,8 @@ export {
   FlowerLotus as LotusIcon,
   NotePencil as NotePencilIcon,
   Bell as BellIcon,
+  User as UserIcon,
+  Bookmarks as BookmarksIcon,
+  GearSix as GearIcon,
+  Plugs as SignOutIcon,
 } from "@phosphor-icons/react";
