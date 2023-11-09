e6f7c052c83c8e9ee1eefeb7eb3d38e0b3b5ccc1
mariquegonn <ocatodev@gmail.com>
2023-11-09 10:08:48 -0300

    styles: base-section in routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { BaseSection } from "@/components/my-ui/base-section";
+
 export default function Home() {
-  return <div></div>;
+  return <BaseSection>Home</BaseSection>;
 }
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,5 @@
+import { BaseSection } from "@/components/my-ui/base-section";
+
+export default function ProfilePage() {
+  return <BaseSection>Profile</BaseSection>;
+}
