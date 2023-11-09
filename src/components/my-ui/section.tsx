b5080495f4b990d9476e313ee6179b75198400b7
mariquegonn <ocatodev@gmail.com>
2023-11-09 10:06:24 -0300

    feat: base-section created

---
+++ src/components/my-ui/section.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+import { ClassNameValue } from "tailwind-merge";
+
+type BaseSectionProps = {
+  className?: ClassNameValue;
+  children: React.ReactNode;
+};
+
+const BaseSection = ({ className, children }: BaseSectionProps) => {
+  return <section className={cn("pt-[56px]", className)}>{children}</section>;
+};
+
+export { BaseSection };
