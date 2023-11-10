44c9fb6a24ac021b691ae6a4453368c54213e878
mariquegonn <ocatodev@gmail.com>
2023-11-10 19:54:00 -0300

    feat: next updated and add basePath in SessionProvider

--- src/providers/auth.tsx
+++ src/providers/auth.tsx
@@ -3,5 +3,9 @@
 import { SessionProvider, SessionProviderProps } from "next-auth/react";
 
 export const AuthProvider = ({ children }: SessionProviderProps) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider basePath="https://lotus-medium.vercel.app/api/auth">
+      {children}
+    </SessionProvider>
+  );
 };
